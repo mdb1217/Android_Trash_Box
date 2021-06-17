92e3c6eee5a6fc8ba78ae23f583faa4017516d4d
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-06-17 16:46:04 +0900

    [ADD] add main activity's res id and viewmodel

--- CustomCalendar/app/src/main/java/org/sopt/customcalendar/ui/MainActivity.kt
+++ CustomCalendar/app/src/main/java/org/sopt/customcalendar/ui/MainActivity.kt
@@ -1,12 +1,27 @@
 package org.sopt.customcalendar.ui
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.customcalendar.R
+import org.sopt.customcalendar.databinding.ActivityMainBinding
+import org.sopt.customcalendar.ui.base.BindingActivity
+import org.sopt.customcalendar.ui.viewmodel.MainViewModel
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+@AndroidEntryPoint
+class MainActivity : BindingActivity<ActivityMainBinding, MainViewModel>() {
+    override val layoutResourceId: Int
+        get() = R.layout.activity_main
+    override val viewModel: MainViewModel by viewModels()
+
+    override fun initView() {
+
+    }
+
+    override fun initBeforeBinding() {
+
+    }
+
+    override fun initAfterBinding() {
+
     }
 }
