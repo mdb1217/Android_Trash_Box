51d6521dc469acf3209f274d8a56d459c9314f72
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-06-20 20:33:07 +0900

    [ADD] add database module

--- CustomCalendar/app/src/main/java/org/sopt/customcalendar/di/DataBaseModule.kt
+++ CustomCalendar/app/src/main/java/org/sopt/customcalendar/di/DataBaseModule.kt
@@ -1,4 +1,29 @@
 package org.sopt.customcalendar.di
 
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import org.sopt.customcalendar.data.dao.CalendarDao
+import org.sopt.customcalendar.data.database.LocalDatabase
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object DataBaseModule {
+    @Provides
+    @Singleton
+    fun provideLocalDatabase(@ApplicationContext context: Context): LocalDatabase =
+        Room.databaseBuilder(
+            context,
+            LocalDatabase::class.java,
+            "local database"
+        ).fallbackToDestructiveMigration()
+            .build()
+
+    @Provides
+    fun provideProfileDao(database: LocalDatabase): CalendarDao = database.calendarDao
 }
