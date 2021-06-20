accd5a24e115a53dff6c741df2d98d41308b7c9a
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-06-20 20:32:46 +0900

    [ADD] add type converter

---
+++ CustomCalendar/app/src/main/java/org/sopt/customcalendar/data/converter/Converter.kt
@@ -0,0 +1,12 @@
+package org.sopt.customcalendar.data.converter
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+
+class Converter {
+    @TypeConverter
+    fun listToJson(value: List<String>?) = Gson().toJson(value)
+
+    @TypeConverter
+    fun jsonToList(value: String) = Gson().fromJson(value, Array<String>::class.java).toList()
+}
