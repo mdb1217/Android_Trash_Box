6f77cc42e7c98e9e5702454740edfc9e45b6c844
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-06-22 16:19:18 +0900

    [ADD] add firebase push notification service

--- Notification/app/src/main/java/org/sopt/notification/notification/SampleMessagingService.kt
+++ Notification/app/src/main/java/org/sopt/notification/notification/SampleMessagingService.kt
@@ -1,14 +1,60 @@
 package org.sopt.notification.notification
 
+import android.R
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import org.sopt.notification.MainActivity
+
 
 class SampleMessagingService : FirebaseMessagingService() {
-    override fun onNewToken(token: String) {
-        
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d("gasdgds", "성공")
+            sendMainNotification(remoteMessage)
+        }
     }
 
-    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+    private fun sendMainNotification(remoteMessage: RemoteMessage) {
+        val uniId = remoteMessage.sentTime.toInt()
 
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("isOpenFromPushAlarm", true)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(
+            this, uniId, intent, PendingIntent.FLAG_ONE_SHOT
+        )
+
+        val channelId = "노티피케이션 메시지"
+
+        val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+        val notificationBuilder =
+            NotificationCompat.Builder(this, channelId).setSmallIcon(R.drawable.ic_secure)
+                .setContentTitle(remoteMessage.data["title"].toString())
+                .setContentText(remoteMessage.data["body"].toString())
+                .setAutoCancel(true)
+                .setSound(soundUri)
+                .setContentIntent(pendingIntent)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel =
+                NotificationChannel(channelId, "Notice", NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(uniId, notificationBuilder.build())
     }
 }
